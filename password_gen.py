dab6f05edf05bd8ae5d1f38eca4632f89ed08f42
Aiman <aimanuddin@gmail.com>
2021-05-08 14:47:34 +0900

    Update password_gen.py

--- password_gen.py
+++ password_gen.py
@@ -1,8 +1,9 @@
 import random
 
 print("Yeehaww welcome to PASSWORD GENERATOR ! ")
+
 # chars = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM!@#$-?_&(){]}[1234567890'
-chars = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM!@#$%^&*()1234567890'
+chars = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM!@#$%^&*()1234567890'                  # some websites requires different type of characters
 # chars = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM@#$%1234567890'
 # chars = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890'
 
@@ -15,11 +16,12 @@ main_list = []
 for password in range(number):
     pwd = ''
     for c in range(length):
-        pwd += random.choice(chars)
+        pwd += random.choice(chars)            
     main_list.append(pwd + '\n')
 
-print(main_list)
+print(main_list) #print passwords on the terminal
 
+## Generated passwords will be save as text file
 with open('generated_passwords.txt','w') as output:
     for item in main_list:
         output.write(item)
