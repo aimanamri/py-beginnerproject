54d34b5d2ecfc1f8229359a2edc69e06f1a9c4e9
Aiman Amri <aimanuddin@gmail.com>
2021-06-12 01:46:54 +0900

    Rewrite the script (recursively)

--- 04_CountdownTimer/timer.py
+++ 04_CountdownTimer/timer.py
@@ -1,23 +1,30 @@
 import time
-'''IN PROGRESSS ---NOT WORKING'''
-input_time = int(input('Set timer for how long?'))
-print('If everything all right, start now ? (y/n)')
-start = input('')
 
-while True:
-    if start == 'y':
-        # print('START NOW !!!')
-        for i in range(input_time,0):
-            print(i)
-            i-=1
-            if i == 0:
-                break
-    elif start == 'n':
-        input_time = input('Set timer for how long?')
-        print('If everything all right, start now ? (y/n) ')
-        start = input('')
+def countdown():
+    def confirm():
+        start = input('If everything all right, start now ? (y/n) \n') # Confirmation before timer starts. User can reset the timer
         if start == 'y':
-            print('START NOW !!!')
+            return True
+        elif start == 'n':
+            return False
 
-for i in range(input_time*60):
-    print(countdown = i-1)
+    start = False
+    while True: # MAIN LOOP
+        INPUT_TIME = input('Set timer for how long (minutes) ?  \n')
+        start = confirm()
+        if start == True:
+            INPUT_TIME=int(INPUT_TIME)*60
+            while INPUT_TIME: # INNER LOOP
+                mins,secs = divmod(INPUT_TIME,60) # Unpack the tuple (quotient,remainder)
+                timer = '{:02d}:{:02d}'.format(mins, secs)
+                print(timer, end="\r") # the next line printed will overwrite the previous one.
+                time.sleep(1)
+                INPUT_TIME -= 1 # Break inner loop when timer reaches 00:00
+            print('TIMER ENDED')
+            break # Break the MAIN LOOP
+            
+        elif start == False:
+            countdown()
+
+if __name__ == '__main__':
+    countdown()
