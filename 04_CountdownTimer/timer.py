e068135f5071babab04595bd47cb03d04ca2c8c3
Aiman Amri <aimanuddin@gmail.com>
2021-06-12 02:06:26 +0900

    Replace break with return, remove while main loop

--- 04_CountdownTimer/timer.py
+++ 04_CountdownTimer/timer.py
@@ -9,22 +9,21 @@ def countdown():
             return False
 
     start = False
-    while True: # MAIN LOOP
-        INPUT_TIME = input('Set timer for how long (minutes) ?  \n')
-        start = confirm()
-        if start == True:
-            INPUT_TIME=int(INPUT_TIME)*60
-            while INPUT_TIME: # INNER LOOP
-                mins,secs = divmod(INPUT_TIME,60) # Unpack the tuple (quotient,remainder)
-                timer = '{:02d}:{:02d}'.format(mins, secs)
-                print(timer, end="\r") # the next line printed will overwrite the previous one.
-                time.sleep(1)
-                INPUT_TIME -= 1 # Break inner loop when timer reaches 00:00
-            print('TIMER ENDED')
-            break # Break the MAIN LOOP
-            
-        elif start == False:
-            countdown()
+    INPUT_TIME = input('Set timer for how long (minutes) ?  \n')
+    start = confirm()
+    if start == True:
+        INPUT_TIME=int(INPUT_TIME)*60
+        while INPUT_TIME: 
+            mins,secs = divmod(INPUT_TIME,60) # Unpack the tuple (quotient,remainder)
+            timer = '{:02d}:{:02d}'.format(mins, secs)
+            print(timer, end="\r") # the next line printed will overwrite the previous one.
+            time.sleep(1)
+            INPUT_TIME -= 1 # Break inner loop when timer reaches 00:00
+        print('TIMER ENDED')
+        return 
+        
+    elif start == False:
+        return countdown()
 
 if __name__ == '__main__':
     countdown()
